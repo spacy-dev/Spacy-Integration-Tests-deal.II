3f6638e5977d414d74a0e5c9fe52919af3df895a
Lars Lubkoll <lars.lubkoll@posteo.de>
2018-11-16 21:23:04 +0100

    Install boost-dependencies via Spacy-script

--- run_test_in_docker.sh
+++ run_test_in_docker.sh
@@ -12,14 +12,6 @@ mkdir $LIB_DIR
 export PATH=$PATH:$INCLUDE_DIR:$LIB_DIR
 
 cd $DEPS
-git clone --recursive https://github.com/boostorg/boost.git
-cd boost
-git checkout master
-./bootstrap.sh --prefix=$HOME
-./b2 headers
-./b2 install
-
-cd $DEPS
 git clone https://github.com/google/googletest.git
 cd googletest
 mkdir build && cd build
@@ -33,6 +25,9 @@ cp -r FunG/fung $HOME/include
 
 cd $DEPS
 git clone https://github.com/spacy-dev/Spacy
+
+Spacy/Scripts/install_boost_submodules $HOME
+
 cd Spacy
 mkdir build && cd build
 cmake .. -DdealII=ON -DBuildTest=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$HOME/lib
