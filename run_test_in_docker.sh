a821f812694f0aa79133594572f52f1be06428ab
Lars Lubkoll <lars.lubkoll@posteo.de>
2018-12-01 10:42:50 +0100

    Use custom docker image

--- run_test_in_docker.sh
+++ run_test_in_docker.sh
@@ -2,37 +2,43 @@
 
 DEPS=$HOME/deps
 INCLUDE_DIR=$HOME/include
-LIB_DIR=$HOME/lib
+INSTALL_DIR=$HOME/install
 TEST_DIR=$HOME/test
 TRAVIS_DIR=$HOME/travis
 
 mkdir $DEPS
 mkdir $INCLUDE_DIR
-mkdir $LIB_DIR
-export PATH=$PATH:$INCLUDE_DIR:$LIB_DIR
+mkdir $INSTALL_DIR
+export PATH=$PATH:$INCLUDE_DIR:$INSTALL_DIR
 
 cd $DEPS
 git clone https://github.com/google/googletest.git
 cd googletest
 mkdir build && cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=$HOME
+cmake .. -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR
 cmake --build .
 cmake --build . --target install
 
 cd $DEPS
 git clone https://github.com/lubkoll/FunG
-cp -r FunG/fung $HOME/include
+cd FunG && mkdir build && build
+cmake .. && make install
 
 cd $DEPS
 git clone https://github.com/spacy-dev/Spacy
-
-Spacy/Scripts/install_boost_submodules $HOME
-
 cd Spacy
 mkdir build && cd build
-cmake .. -DdealII=ON -DBuildTest=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$HOME/lib
+cmake .. -DdealII=ON -DBuildTest=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR
 cmake --build .
 cmake --build . --target install
+
+cd $DEPS/Spacy/Examples/deal.II_Tutorial/Tutorial/Step4 
+mkdir build && cd build 
+cmake .. && cmake --build . && ./run
+
+cd $DEPS/Spacy/Examples/deal.II_Tutorial/Tutorial_Spacy/Step4 
+mkdir build && cd build 
+cmake .. && cmake --build . && ./run
 
 mkdir $TEST_DIR && cd $TEST_DIR
 mkdir build && cd build
