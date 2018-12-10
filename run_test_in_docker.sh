8b692b27c29a653b213ecc83221f533ad96c0e06
Lars Lubkoll <lars.lubkoll@posteo.de>
2018-12-10 21:56:00 +0100

    Remove cmake installation

--- run_test_in_docker.sh
+++ run_test_in_docker.sh
@@ -25,28 +25,24 @@ cd FunG && mkdir build && build
 cmake .. && make install
 
 cd $DEPS
-wget http://github.com/Kitware/CMake/releases/download/v3.13.1/cmake-3.13.1.tar.gz
-tar xzf cmake-3.13.1.tar.gz && cd cmake-3.13.1 && ./bootstrap --prefix=$DEPS && make && make install
-
-cd $DEPS
 git clone https://github.com/spacy-dev/Spacy
 cd Spacy
 mkdir build && cd build
-$DEPS/bin/cmake .. -DdealII=ON -DBuildTest=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR
-$DEPS/bin/cmake --build .
-$DEPS/bin/cmake --build . --target install
+cmake .. -DdealII=ON -DBuildTest=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR
+cmake --build .
+cmake --build . --target install
 
 cd $DEPS/Spacy/Examples/deal.II_Tutorial/Tutorial/Step4 
 mkdir build && cd build 
-$DEPS/bin/cmake .. && $DEPS/bin/cmake --build . && ./run
+cmake .. && cmake --build . && ./run
 
 cd $DEPS/Spacy/Examples/deal.II_Tutorial/Tutorial_Spacy/Step4 
 mkdir build && cd build 
-$DEPS/bin/cmake .. && $DEPS/bin/cmake --build . && ./run
+cmake .. && cmake --build . && ./run
 
 mkdir $TEST_DIR && cd $TEST_DIR
 mkdir build && cd build
-$DEPS/bin/cmake $TRAVIS_DIR -DdealII=ON -DCMAKE_BUILD_TYPE=Release
-$DEPS/bin/cmake --build .
+cmake $TRAVIS_DIR -DdealII=ON -DCMAKE_BUILD_TYPE=Release
+cmake --build .
 
 ctest
