6211a88082289b614c386a670586ce4ff35a4f49
Lars Lubkoll <lars.lubkoll@posteo.de>
2018-12-12 20:00:53 +0100

    export gtest_root

--- run_test_in_docker.sh
+++ run_test_in_docker.sh
@@ -16,8 +16,8 @@ git clone https://github.com/google/googletest.git
 cd googletest
 mkdir build && cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR
-cmake --build .
 cmake --build . --target install
+export GTEST_ROOT=$INSTALL_DIR
 
 cd $DEPS
 git clone https://github.com/lubkoll/FunG
@@ -28,7 +28,6 @@ git clone https://github.com/spacy-dev/Spacy
 cd Spacy
 mkdir build && cd build
 cmake .. -DdealII=ON -DBuildTest=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR
-cmake --build .
 cmake --build . --target install
 
 cd $DEPS/Spacy/Examples/deal.II_Tutorial/Tutorial/Step4 
@@ -41,7 +40,8 @@ cmake .. && cmake --build . && ./run
 
 mkdir $TEST_DIR && cd $TEST_DIR
 mkdir build && cd build
-cmake $TRAVIS_DIR -DdealII=ON -DCMAKE_BUILD_TYPE=Release
+cmake $TRAVIS_DIR -DCMAKE_BUILD_TYPE=Release
 cmake --build .
 
-ctest
+./dealII_TutorialsTest
+#ctest
