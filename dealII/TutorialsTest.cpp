bc2b6fa6b014ac63413b8ba37751b152e5f09899
Lars Lubkoll <lars.lubkoll@posteo.de>
2021-03-10 19:46:00 +0100

    Add dependencies using conan

--- dealII/TutorialsTest.cpp
+++ dealII/TutorialsTest.cpp
@@ -4,7 +4,7 @@
 
 TEST(dealii_Tutorial, Step4)
 {
-    const auto referenceFileName = "/home/deps/Spacy/Examples/deal.II_Tutorial/Tutorial/Step4/build/solution-2d.vtk";
-    const auto solutionFileName = "/home/deps/Spacy/Examples/deal.II_Tutorial/Tutorial_Spacy/Step4/build/sol.vtk";
+    const auto referenceFileName = "/home/dealii/deps/Spacy/Examples/deal.II_Tutorial/Tutorial/Step4/build/solution-2d.vtk";
+    const auto solutionFileName = "/home/dealii/deps/Spacy/Examples/deal.II_Tutorial/Tutorial_Spacy/Step4/build/sol.vtk";
     EXPECT_TRUE(Spacy::compareLookupTable(referenceFileName, solutionFileName));
 }
--- run_test_in_docker.sh
+++ run_test_in_docker.sh
@@ -1,33 +1,45 @@
 #!/bin/bash
 
-DEPS=/home/deps
-INCLUDE_DIR=/home/include
-INSTALL_DIR=/home/install
-TEST_DIR=/home/test
+set -e
+
+sudo apt update
+sudo apt install python3 python3-pip python3-venv liblapacke-dev -y
+python3 -m pip install --user pipx
+python3 -m pipx ensurepath
+export PATH="${PATH}:/home/dealii/.local/bin"
+pipx install conan
+conan profile new default
+conan profile update settings.compiler=gcc default
+conan profile update settings.compiler.version=9 default
+conan profile update settings.arch=x86_64 default
+conan profile update settings.os=Linux default
+conan profile update settings.compiler.libcxx=libstdc++11 default
+
+sudo update-alternatives --install /usr/local/bin/python python /usr/bin/python3 99
+
+DEPS=/home/dealii/deps
+INCLUDE_DIR=/home/dealii/include
+INSTALL_DIR=/home/dealii/install
+TEST_DIR=/home/dealii/test
 TRAVIS_DIR=/home/travis
 
+mkdir -p /home/dealii
 mkdir $DEPS
 mkdir $INCLUDE_DIR
 mkdir $INSTALL_DIR
 export PATH=$PATH:$INCLUDE_DIR:$INSTALL_DIR
 
 cd $DEPS
-git clone https://github.com/google/googletest.git
-cd googletest
-mkdir build && cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR
-cmake --build . --target install
-export GTEST_ROOT=$INSTALL_DIR
-
-cd $DEPS
 git clone https://github.com/lubkoll/FunG
 cd FunG && mkdir build && cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR && cmake --build . --target install
+
 cd $DEPS
 git clone https://github.com/spacy-dev/Spacy
 cd Spacy
 mkdir build && cd build
-cmake .. -DdealII=ON -DBuildTest=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR
+conan install /home/spacy --build=missing
+cmake /home/spacy -DdealII=ON -DBuildTest=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR -DCMAKE_TOOLCHAIN_FILE=conan_paths.cmake
 cmake --build . --target install
 
 cd $DEPS/Spacy/Examples/deal.II_Tutorial/Tutorial/Step4 
@@ -40,7 +52,8 @@ cmake .. && cmake --build . && ./run
 
 mkdir $TEST_DIR && cd $TEST_DIR
 mkdir build && cd build
-cmake $TRAVIS_DIR -DCMAKE_BUILD_TYPE=Release
+conan install $TRAVIS_DIR
+cmake $TRAVIS_DIR -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=conan_paths.cmake
 cmake --build .
 
 ctest
